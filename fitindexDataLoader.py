4f93f46431bde994f9d9eafb1dde7e44f4fedc3e
Justin Szaro <justinszaro@gmail.com>
2021-11-24 21:51:34 -0500

    Did some light refactoring and removed a redundent function

--- fitindexDataLoader.py
+++ fitindexDataLoader.py
@@ -1,22 +1,18 @@
 from SQLConnect import SQLConnect
 
+
 def getDateAndValue(line):
     components = line.split()
     return [components[7].split('=')[1].strip('"'), components[-1][7:len(components[-1])-2].strip('"')]
 
 
-def insertIntoDict(date, value, dict):
+def insertIntoDict(line, dict):
+    date, value = getDateAndValue(line)
     if date in dict.keys():
-        dict[date] = str((float(dict[date]) + value / 2))
+        dict[date] = str((float(dict[date]) + float(value) / 2))
     else:
         dict[date] = str(value)
     return dict
-
-
-def addToDictionary(line, dictionary):
-    date, value = getDateAndValue(line)
-    dictionary = insertIntoDict(date, float(value), dictionary)
-    return dictionary
 
 
 def getDataType(line):
@@ -29,15 +25,15 @@ def getData(filename):
         for line in in_file:
             data_type = getDataType(line)
             if data_type == "BodyMassIndex":
-                bodyMassIndex = addToDictionary(line, bodyMassIndex)
+                bodyMassIndex = insertIntoDict(line, bodyMassIndex)
             elif data_type == "BodyMass":
-                bodyMass = addToDictionary(line, bodyMass)
+                bodyMass = insertIntoDict(line, bodyMass)
             elif data_type == "BodyFatPercentage":
-                bodyFatPercentage = addToDictionary(line, bodyFatPercentage)
+                bodyFatPercentage = insertIntoDict(line, bodyFatPercentage)
             elif data_type == 'LeanBodyMass':
-                leanBodyMass = addToDictionary(line, leanBodyMass)
+                leanBodyMass = insertIntoDict(line, leanBodyMass)
             elif data_type == 'BasalEnergyBurned':
-                basalEnergyBurned = addToDictionary(line, basalEnergyBurned)
+                basalEnergyBurned = insertIntoDict(line, basalEnergyBurned)
     return [bodyMassIndex, bodyMass, bodyFatPercentage, leanBodyMass, basalEnergyBurned]
 
 
@@ -46,17 +42,13 @@ def getAllDates(bodyMassIndex, bodyMass, bodyFatPercentage, leanBodyMass, basalE
     return dates.union(bodyMassIndex.keys(), bodyMass.keys(), bodyFatPercentage.keys(), leanBodyMass.keys(), basalEnergyBurned.keys())
 
 
-def quotes(string):
-    return '"' + string + '"'
-
-
 def addDataToSQL(dates, bodyMassIndex, bodyMass, bodyFatPercentage, leanBodyMass, basalEnergyBurned):
     connector = SQLConnect()
-    connector.useDatabase('Health')
-    connector.create_table('fitindex', ['date DATE', 'bodyMassIndex FLOAT', 'bodyMass FLOAT', 'bodyFatPercentage FLOAT',
+    connector.useDatabase('health')
+    connector.create_table('Fitindex', ['date DATE', 'bodyMassIndex FLOAT', 'bodyMass FLOAT', 'bodyFatPercentage FLOAT',
                                         'leanBodyMass FLOAT', 'basalEnergyBurned FLOAT'])
     for date in dates:
-        connector.insert_into_table('fitindex', [quotes(date), bodyMassIndex.get(date, '0.0'), bodyMass.get(date, '0.0'),
+        connector.insert_into_table('fitindex', [date, bodyMassIndex.get(date, '0.0'), bodyMass.get(date, '0.0'),
                                                  bodyFatPercentage.get(date, '0.0'), leanBodyMass.get(date, '0.0'),
                                                  basalEnergyBurned.get(date, '0.0')])
     connector.commit()
--- hydrateDataLoader.py
+++ hydrateDataLoader.py
@@ -1,15 +1,15 @@
 from SQLConnect import SQLConnect
 
+
 def convertToFlOz(value):
     return float(value) * 0.033814
 
 
 def insertIntoDict(dictionary, date, value):
     if dictionary.get(date, None) is None:
-        dictionary[date] = value
+        dictionary[date] = value.__round__(2)
     else:
-        dictionary[date] += value
-    dictionary[date] = dictionary[date].__round__(2)
+        dictionary[date] = (dictionary[date] + value).__round__(2)
     return dictionary
 
 
@@ -24,16 +24,12 @@ def getData(filename):
     return hydrateData
 
 
-def quotes(string):
-    return '"' + string + '"'
-
-
 def toSQL(data):
     connector = SQLConnect()
-    connector.useDatabase('Health')
-    connector.create_table('hidrate', ['date CHAR(10), amount FLOAT'])
+    connector.useDatabase('health')
+    connector.create_table('Hidrate', ['date CHAR(10), amount FLOAT'])
     for key in data.keys():
-        connector.insert_into_table('hidrate', [quotes(key), str(data[key])])
+        connector.insert_into_table('hidrate', [key, str(data[key])])
     connector.commit()
 
 
